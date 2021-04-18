86955784b0f9f90ba17c255f2abd2f69be0d619d
elthrasher <elthrasher@gmail.com>
2021-04-18 10:34:25 -0400

    store authz for later

--- cdk/proxy-cors-httpapi-stack.ts
+++ cdk/proxy-cors-httpapi-stack.ts
@@ -1,9 +1,9 @@
-import { CorsHttpMethod, HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2';
+import { CfnAuthorizer, CorsHttpMethod, HttpApi, HttpAuthorizerType, HttpMethod } from '@aws-cdk/aws-apigatewayv2';
 import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 import { LogGroup, RetentionDays } from '@aws-cdk/aws-logs';
-import { Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { Construct, Names, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
 import { join } from 'path';
 
 export class ProxyCorsHttpapiStack extends Stack {
@@ -28,6 +28,29 @@ export class ProxyCorsHttpapiStack extends Stack {
       corsPreflight: { allowMethods: [CorsHttpMethod.GET], allowOrigins: ['*'] },
     });
 
-    httpApi.addRoutes({ integration: httpProxyFnIntegration, methods: [HttpMethod.GET], path: '/' });
+    // Not working - see https://github.com/aws/aws-cdk/pull/13181/files
+    const authorizer = new CfnAuthorizer(this, 'HttpAuthz', {
+      apiId: httpApi.apiId,
+      authorizerPayloadFormatVersion: '2.0',
+      authorizerType: HttpAuthorizerType.LAMBDA,
+      authorizerUri: `arn:aws:apigateway:${
+        process.env.CDK_DEFAULT_REGION
+      }:lambda:path/2015-03-31/functions/${'arn:aws:lambda:us-east-1:336848621206:function:authz-proxy-fn'}/invocations`,
+      enableSimpleResponses: true,
+      identitySource: ['$request.header.Authorization'],
+      name: 'HttpAuthz',
+    });
+
+    httpApi.addRoutes({
+      authorizer: {
+        bind: () => ({
+          authorizerId: authorizer.name,
+          authorizationType: HttpAuthorizerType.LAMBDA,
+        }),
+      },
+      integration: httpProxyFnIntegration,
+      methods: [HttpMethod.GET],
+      path: '/',
+    });
   }
 }
