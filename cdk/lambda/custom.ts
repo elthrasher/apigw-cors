a71a1f39f7143e002d9c68765fd0627e7b9d0af7
elthrasher <elthrasher@gmail.com>
2021-04-18 13:22:47 -0400

    remove authz

--- cdk/lambda/custom.ts
+++ cdk/lambda/custom.ts
@@ -1,8 +1,7 @@
 interface inputEvent {
   input: string;
-  name: string;
 }
 
 export const handler = async (event: inputEvent): Promise<string> => {
-  return JSON.stringify({ output: event.input, user: event.name });
+  return JSON.stringify({ output: event.input });
 };
